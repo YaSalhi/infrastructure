974ffea27010999493af3a34488c7e536c91a234
yassine salhi <sayassine54@gmail.com>
2025-10-19 16:45:31 +0100

    Ajout de la base de données Dev

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -28,3 +28,35 @@ resource "azurerm_linux_web_app" "app_dev" {
      always_on = false
   }
 }
+
+  # comme ma suscription ne donne pas la possibilité de crée le bdd dans (Europe) West Europe j'ai choisi  northeurope
+  #il est important de garder les ressources proches (même région ou région proche) pour limiter la latence.
+  # * tjrs vérifier que la location choisi pour votre rg a tous les services de votre projet  
+  variable "sql_location" {
+    default = "northeurope"
+  }
+
+  variable "sql_admin_password" {
+    description = "Mot de passe administrateur SQL"
+    type        = string
+    sensitive   = true
+    default     = "Password123!" # Pour tests seulement, sinon utiliser Key Vault ou secret pipeline
+  }
+
+  # database 
+  resource "azurerm_mssql_server" "sql" {
+  name                         = "server-dev-env"
+  resource_group_name          = azurerm_resource_group.rg_dev.name
+  #location                     = azurerm_resource_group.rg_dev.location *
+  location = var.sql_location
+  version                      = "12.0"
+  administrator_login          = "azureadmin"
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_mssql_database" "db" {
+  name           = "db-dev-env"
+  server_id      = azurerm_mssql_server.sql.id
+  sku_name       = "S0"
+}
+
